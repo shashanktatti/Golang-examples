9a61cffc7f7ca9ea8ffeaf0c4afbc0887043eaee
T D <shashank.t.d@intel.com>
2019-10-28 13:32:48 +0530

    Modification in Loops

--- examples/Loops/forAndwhile.go
+++ examples/Loops/forAndwhile.go
@@ -19,11 +19,24 @@ func main() {
 	fmt.Printf("\n")
 
 	/*
+		OR
+	*/
+
+	fmt.Println("Printing 1 to 10 using for type 2")
+	counter := 1		// Implicit type inference
+	for ;counter <=10; counter++ {
+		fmt.Printf("%d", counter)
+		fmt.Printf(" ")
+	}
+
+	fmt.Printf("\n")
+
+	/*
 		If while loop is to written then for is go's while. There's no while keyword in GO!
 	*/
 
-	fmt.Println("Printing 1 to 10 using while with for")
-	counter := 1		// Implicit type inference
+	fmt.Println("Printing 1 to 10 using while with for type 1")
+	counter = 1		// Notice that counter is declared before, so := is not required.
 	for counter <=10 {
 		fmt.Printf("%d", counter)
 		fmt.Printf(" ")
